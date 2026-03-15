0bfbc0662d8ffbe495dc8d297115459a0aa3b1e2
NicoSciangula <nico.sciangula99@gmail.com>
2026-03-15 15:40:20 +0100

    added img and text

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,27 @@
 export default function HomePage() {
-  return;
+  return (
+    <>
+      <section className="p-3 my-bg mt-3">
+        <h1 className="text-center my-font-color">
+          Benvenuti nel mio E-Commerce
+        </h1>
+        <div>
+          <img
+            src="https://cdn.shopify.com/s/files/1/1354/3229/articles/idee_oggetti_fai_da_te_da_vendere.jpg?v=1727280972"
+            alt="immagine prodotto"
+            className="rounded img-fluid"
+          />
+        </div>
+        <p className="pt-3 fs-4 my-font-color">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+          optio nemo aliquid sint aperiam exercitationem accusamus expedita ab
+          laudantium, placeat pariatur at accusantium nisi fugiat, doloremque
+          rem quod? Doloremque, fugiat. Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Voluptatibus vero quisquam earum quo temporibus
+          commodi, pariatur, reprehenderit hic libero a tempore alias officia
+          harum ad porro, repudiandae illum amet veniam!
+        </p>
+      </section>
+    </>
+  );
 }
