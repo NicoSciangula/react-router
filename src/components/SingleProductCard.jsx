e1d8c8272993d0275dc8431026ca2186cf7ee567
NicoSciangula <nico.sciangula99@gmail.com>
2026-03-15 18:52:26 +0100

    added link to go in the more info card

--- src/components/SingleProductCard.jsx
+++ src/components/SingleProductCard.jsx
@@ -1,15 +1,18 @@
-export default function SingleProductCard({products}) {
-  const {title, image, description, price, category} = products;
+import {Link} from "react-router-dom";
+
+export default function SingleProductCard({products, isDetail}) {
+  const {title, image, description, price, category, id} = products;
 
   return (
-    <article className="p-3">
+    <article className="p-3 d-flex justify-content-center align-items-start">
       <div
-        className="card overflow-hidden my-product p-3"
-        style={{width: "18rem", height: "26rem"}}>
+        className="card overflow-hidden p-3"
+        style={{width: "18rem", height: "100%"}}>
         <img src={image} className="card-img-top my-img" alt={title} />
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
-          <p className="card-text my-description-product my-font-color">
+          <p
+            className={`card-text ${isDetail === false ? "my-description-product" : "d-block"} my-font-color`}>
             {description}
           </p>
           <div className="my-bg text-center">
@@ -17,6 +20,15 @@ export default function SingleProductCard({products}) {
             <span className="card-text my-font-color">{category}</span>
           </div>
         </div>
+        {isDetail === false ? (
+          <Link to={`/product/${id}`} className="btn btn-primary">
+            View More
+          </Link>
+        ) : (
+          <Link to="/product" className="btn btn-primary">
+            Torna alla Lista
+          </Link>
+        )}
       </div>
     </article>
   );
