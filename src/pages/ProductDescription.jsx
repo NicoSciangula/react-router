af924ba30edf804094f0787fc57932a8c8d4fc91
NicoSciangula <nico.sciangula99@gmail.com>
2026-03-15 19:30:43 +0100

    added useNavigate and a dependency to useEffect

--- src/pages/ProductDescription.jsx
+++ src/pages/ProductDescription.jsx
@@ -1,22 +1,28 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
+import {useParams, useNavigate} from "react-router-dom";
 import SingleProductCard from "../components/SingleProductCard";
 
 export default function ProductDescription() {
   const [singleProduct, setSingleProduct] = useState([]);
   const {id} = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios
       .get(`https://fakestoreapi.com/products/${id}`)
       .then((res) => {
-        setSingleProduct(res.data);
+        if (!res.data) {
+          console.warn("Prodotto non trovato, reinderizzo...");
+          navigate("/product");
+        } else {
+          setSingleProduct(res.data);
+        }
       })
       .catch((error) => {
         console.error(error);
       });
-  }, []);
+  }, [id]);
 
   return (
     <>
