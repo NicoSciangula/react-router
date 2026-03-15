76ae7fab6c0ff98a6e760146087fbdca87475d20
NicoSciangula <nico.sciangula99@gmail.com>
2026-03-15 16:08:26 +0100

    added axios get

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,1 +1,31 @@
-export default function Product() {}
+import axios from "axios";
+import SingleProduct from "../components/SingleProduct";
+import {useEffect, useState} from "react";
+
+export default function Product() {
+  const [product, setProduct] = useState([]);
+  function getProduct() {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((res) => {
+        console.log(res.data);
+        setProduct(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  return (
+    <>
+      <section className="my-bg my-font-color mt-3 p-3">
+        <h1 className="text-center">I nostri Prodotti</h1>
+        <div></div>
+      </section>
+    </>
+  );
+}
